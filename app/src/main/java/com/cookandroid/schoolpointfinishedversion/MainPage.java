153c29a2749344f334408cd77b10766b981b3400
MoneyAdd <tjdwns0158@naver.com>
2022-11-27 14:50:06 +0900

    add

--- app/src/main/java/com/cookandroid/schoolpointfinishedversion/MainPage.java
+++ app/src/main/java/com/cookandroid/schoolpointfinishedversion/MainPage.java
@@ -2,38 +2,53 @@ package com.cookandroid.schoolpointfinishedversion;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.health.SystemHealthManager;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainPage extends AppCompatActivity {
-BottomNavigationView bottomNavigationView;
+    BottomNavigationView bottomNavigationView;
+
+    MainpageFragment mainpageFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
 
-      bottomNavigationView = findViewById(R.id.bottomnavi);
+        bottomNavigationView = findViewById(R.id.bottomnavi);
+        mainpageFragment = new MainpageFragment();
 
-      //처음화면
-        getSupportFragmentManager().beginTransaction().add(R.id.mainframe, new MainpageFragment()).commit();
+        //처음화면
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainframe, mainpageFragment).commitAllowingStateLoss();
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
                 switch (item.getItemId()){
                     case R.id.home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.mainframe, new MainpageFragment()).commit();
-                        break;
+                        getSupportFragmentManager().beginTransaction().replace(R.id.mainframe, mainpageFragment).commitAllowingStateLoss();
+                        return true;
                 }
-
-
                 return true;
             }
         });
 
+
+
     }
+
+
 }
--- app/src/main/java/com/cookandroid/schoolpointfinishedversion/MainpageFragment.java
+++ app/src/main/java/com/cookandroid/schoolpointfinishedversion/MainpageFragment.java
@@ -1,20 +1,56 @@
 package com.cookandroid.schoolpointfinishedversion;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainpageFragment extends Fragment {
+
+    View view;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.mainpage, container, false);
+
+        view = inflater.inflate(R.layout.mainpage, container, false);
+        TextView time = view.findViewById(R.id.tv_mainpage_time);
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                time.setText(DateFormat.getDateTimeInstance().format(new Date()));
+            }
+        };
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                while (true){
+                    try {
+                        Thread.sleep(1000);
+                    }catch (InterruptedException e){ }
+                    handler.sendEmptyMessage(1);
+                }
+            }
+        };
+        Thread thread = new Thread(task);
+        thread.start();
+        return view;
     }
-}
+
+    }
+
+
